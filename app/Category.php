719a6236f2f7715bad0c8eda49c44beb26e14ddc
icaz <icaz.mailg@gmail.com>
2020-02-13 16:36:37 +0100

    dashsidebar

--- app/Category.php
+++ app/Category.php
@@ -24,9 +24,13 @@ class Category extends Model
             ]
         ];
     }    
+    public function posts()
+    {
+        return $this->belongsToMany('App\Post', 'category_posts')->orderBy('created_at','DESC')->paginate(5);
+    }
+
     public function getRouteKeyName()
     {
     	return 'slug';
     }
-
 }
--- app/Post.php
+++ app/Post.php
@@ -3,8 +3,43 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
+
 
 class Post extends Model
 {
-    //
+    protected $guarded = [];
+
+    
+    use Sluggable;
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }    
+    
+    public function tags()
+    {
+        return $this->belongsToMany('App\Tag', 'post_tags')->withTimestamps();
+    }
+    public function categories()
+    {
+        return $this->belongsToMany('App\Category', 'category_posts')->withTimestamps();
+    }
+
+
+    public function getRouteKeyName()
+    {
+    	return 'slug';
+    }
+
 }
